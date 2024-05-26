ee4431bcc30f89c98fc6ec095781ebfe9083ed5d
kangbyeonghyeon <rkdqudgus9806@gmail.com>
2024-05-26 15:41:59 +0900

    feat: add movie store

---
+++ src/store/movie.js
@@ -0,0 +1,17 @@
+import { Store } from '../core';
+
+const store = new Store({
+  searchText: '',
+  page: 1,
+  movies: [],
+});
+
+export default store;
+
+export const searchMovies = async page => {
+  const res = await fetch(
+    `https://omdbapi.com?apikey=7035c60c&s=${store.state.searchText}&page=${page}`,
+  );
+  const json = await res.json();
+  console.log(json);
+};
